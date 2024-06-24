ff425c1bc9c3d195b76a6a015df1e2278c668596
djwatson13 <watsontrombone13@gmail.com>
2024-06-23 19:59:05 -0500

    followed instructions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+function createMessage() {
+    let message = {};
+    message.sender = prompt("What is your name?");
+    message.recipient = prompt("Who will you send the message to?")
+    message.request = prompt("What do you want your class to stop?");
+  
+    myMessage.innerHTML = `Hello ${message.recipient}, I want to beg you to ${message.request}. These assignments take me a week to do because of this. -- ${message.sender}`
+  
+    myHeader.style.color = "skyblue";
+    myHeader.style.backgroundColor = "black";
+    myMessage.style.color = "skyblue";
+    myMessage.style.backgroundColor = "black";
+  }
+  
+  messageButton.onclick = createMessage;
